c035181f18f6b3c60b81ee92f1b5be6d9f659041
Aratamotsu <54492243+aratamotsu@users.noreply.github.com>
2021-02-02 08:17:02 +0900

    fix tag's border

--- components/show-tags.js
+++ components/show-tags.js
@@ -5,12 +5,12 @@ export default function ShowTags({ tags }) {
         <div className="text-sm">
             {!Array.isArray(tags) ? (
                 <Link as={`/tags/${tags}`} href="/tags/[tags]">
-                    <a className="px-2 pt-0.5 mr-2 border-solid border-2 border-black hover:bg-gray-200 rounded-lg">{tags}</a>
+                    <a className="px-2 pt-0.5 mr-2 border-solid border border-black hover:bg-gray-200 rounded-md">{tags}</a>
                 </Link>
             ) : (
                 tags.map((tag) => (
                     <Link key={tag} as={`/tags/${tag}`} href="/tags/[tag]">
-                        <a className="px-2 pt-0.5 mr-2 border-solid border-2 border-black hover:bg-gray-200 rounded-lg">{tag}</a>
+                        <a className="px-2 pt-0.5 mr-2 border-solid border border-black hover:bg-gray-200 rounded-md">{tag}</a>
                     </Link>
                 ))
             )}
